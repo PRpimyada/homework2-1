92fcb9ff1c286d6c589582a3a17dfe3c27cddf52
PRpimyada <123532151+PRpimyada@users.noreply.github.com>
2023-05-19 23:26:58 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int rowNumber = ReadRowNumber();
+        Console.WriteLine("Pascal's Triangle:");
+
+        for (int i = 0; i <= rowNumber; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write(CalculatePascalValue(i, j) + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+    static int ReadRowNumber()
+    {
+        Console.Write("Enter the Pascal's triangle row number: ");
+        int rowNumber;
+
+        while (!int.TryParse(Console.ReadLine(), out rowNumber) || rowNumber < 0)
+        {
+            Console.WriteLine("Invalid Pascal's triangle row number.");
+            Console.Write("Enter the Pascal's triangle row number again: ");
+        }
+
+        return rowNumber;
+    }
+    static int CalculatePascalValue(int row, int column)
+    {
+        if (column == 0 || column == row)
+        {
+            return 1;
+        }
+
+        return CalculatePascalValue(row - 1, column - 1) + CalculatePascalValue(row - 1, column);
+    }
+}
